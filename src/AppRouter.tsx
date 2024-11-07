34bec2b12833f2687577126b7b730f7009697858
IngSystemCix <juanromerocollazos@gmail.com>
2024-11-07 08:20:29 -0500

    update: change in files of routes and error 404

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -6,7 +6,7 @@ export const AppRouter = () => {
     return(
         <BrowserRouter>
             <RoutesWithNotFound>
-                <Route path="*" element={<Navigate to={"/home"} />} />
+                <Route path="/" element={<Navigate to={"/home"} />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
--- src/public/NotFound/NotFound.tsx
+++ src/public/NotFound/NotFound.tsx
@@ -1,7 +1,13 @@
+import './NotFound.css';
+
 export const NotFound = () => {
     return (
-        <div>
-            <h1>404 Not Found</h1>
-        </div>
+        <main>
+            <figure>
+                <img src="/404.png" alt="error 404"/>
+                <figcaption>Página no encontrada</figcaption>
+            </figure>
+            <a href="/">Regresar a la página principal</a>
+        </main>
     );
 }
